81cbf887520a267b022b4ea52fe3746aee9c1f9d
frankzhang <frank.zhang.sa.au@gmail.com>
2025-01-20 16:20:12 +1000

    1.create data 2. template: use data

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,25 @@
 <!-- 3 main parts -->
- 
-<!--1111111 script -->
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+<!--1111111 script: create data -->
+<script >
+ export default {
+  data(){
+    return {
+      name:"frank",
+      
+    };
+  }
+ }
 </script>
 
 
-<!--222222222   template-->
+<!--222222222   template: use data-->
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{ name }}</h1>
 </template>
 
 
 <!-- 333333333333333       style -->
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style >
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
